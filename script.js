cddaa2c885f4a2a11ad2ec2e89214cf4f22dcc67
Gus <gusr@logicliving.com>
2020-07-23 21:36:49 -0500

    setting up JS

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = documnet.querySelectorAll('[data-number]')
+const operationButtons = documnet.querySelectorAll('[data-operation]')
+const equalButton = documnet.querySelector('[data-equals]')
+const deleteButton = documnet.querySelector('[data-delete]')
+const allClearButton = documnet.querySelector('[data-all-clear]')
+const perviousOperand = documnet.querySelector('[data-previous-operand]')
+const currentOperand = documnet.querySelector('[data-current-operand]')
